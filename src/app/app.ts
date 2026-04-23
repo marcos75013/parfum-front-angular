8aa55acc8a676abc0f23b6f303ee901bdd38af8b
marcos75013 <115374198+marcos75013@users.noreply.github.com>
2026-04-23 10:41:09 +0200

    fix app-footer import component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
-import { AppFooter } from './app-footer/app-footer';
-
+import { AppFooterComponent } from './app-footer/app-footer';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppHeaderComponent, AppFooter],
+  imports: [RouterOutlet, AppHeaderComponent, AppFooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
