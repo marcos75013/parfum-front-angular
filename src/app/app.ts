b4a2f3b875ab971addfed219077d0ee084f5c45a
marcos75013 <115374198+marcos75013@users.noreply.github.com>
2026-04-23 10:52:36 +0200

    app-footer commenté

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppHeaderComponent } from './app-header/app-header.component';
-import { AppFooterComponent } from './app-footer/app-footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppHeaderComponent, AppFooterComponent],
+  imports: [RouterOutlet, AppHeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
